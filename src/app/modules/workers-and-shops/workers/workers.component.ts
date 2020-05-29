044c78ec3a5611cd175dcffdc5c3c0105b22c15d
Timofey Ponomarev <tim99919@gmail.com>
2020-05-30 01:47:04 +0300

    (upd) mock users added. workers component updated. added subscription for data fetching, some logic for user adding.

--- src/app/modules/workers-and-shops/workers/workers.component.ts
+++ src/app/modules/workers-and-shops/workers/workers.component.ts
@@ -1,4 +1,12 @@
-import {ChangeDetectionStrategy, Component, HostBinding, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, HostBinding, OnInit} from '@angular/core';
+import {of} from 'rxjs';
+import MOCK_USERS from '../../../mocks/users';
+
+interface IWorker {
+  id: number;
+  fullName: string;
+  logo: string;
+}
 
 @Component({
   selector: 'super-workers',
@@ -9,12 +17,45 @@ import {ChangeDetectionStrategy, Component, HostBinding, OnInit} from '@angular/
 export class WorkersComponent implements OnInit {
   @HostBinding('class') readonly classes = ['workers'];
 
-  addedWorkers = [1, 2, 3];
+  workers: IWorker[];
 
-  constructor() {
+  private _addedWorkers = new Map<number, IWorker>();
+
+  get addedWorkers(): IWorker[] {
+    return [...this._addedWorkers.values()];
+  }
+
+  constructor(private _cdr: ChangeDetectorRef) {
   }
 
   ngOnInit() {
+    of(MOCK_USERS).subscribe((v: IWorker[]) => {
+      this.workers = v;
+    });
   }
 
+  trackByFn(index: number, worker: IWorker): number {
+    return worker.id;
+  }
+
+  onAddWorkerClick() {
+    const worker = this._getWorker();
+
+    if (!worker) {
+      return;
+    }
+
+    this._addedWorkers.set(worker.id, worker);
+    this._cdr.markForCheck();
+  }
+
+  onWorkerDelete(id: number) {
+    const worker = this._addedWorkers.get(id);
+
+    this.workers.push(worker);
+  }
+
+  private _getWorker() {
+    return this.workers.pop();
+  }
 }
